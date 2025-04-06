4381248d6b451aae0cf8b906be8e5743f7567061
Gurmanpreet Kaur <gurmanpreet341.be22@chitkara.edu.in>
2025-04-06 14:36:36 +0530

    operators

---
+++ Bit Manipulation/operators.java
@@ -0,0 +1,11 @@
+import java.util.* ;
+public class operators {
+    public static void main(String[] args) {
+        System.out.println("AND (&) OPERATOR : " + (13 & 7));
+        System.out.println("OR (|) OPERATOR : " + (13 | 7));
+        System.out.println("XOR (^) OPERATOR : " + (13 ^ 7));
+        System.out.println("RIGHT SHIFT (>>) OPERATOR : " + (13 >> 1));
+        System.out.println("LEFT SHIFT (<<) OPERATOR : " + (13 << 1));
+        System.out.println("NOT (~) OPERATOR : " + (~5));
+    }
+}
